7cac97b74f727fe874a4a182d94aa9b216e4f3dd
ginting talang <gintingtalang1100@gmail.com>
2025-03-13 15:15:29 +0700

    update feature limit

--- model/limit.go
+++ model/limit.go
@@ -5,15 +5,17 @@ import (
 )
 
 type Limit struct {
-	LimitID        uint       `gorm:"primaryKey" json:"limit_id"`
-	LimitNIK       string     `gorm:"not null" json:"limit_nik"`
-	NIKCustomer    Customer   `gorm:"foreignKey:LimitNIK;references:CustomerNIK;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
-	LimitTenor     int        `gorm:"not null" json:"limit_tenor"`
-	LimitAmount    float64    `gorm:"not null" json:"limit_amount"`
-	LimitCreatedBy uint       `gorm:"not null" json:"limit_created_by"`
-	CreatedByUser  User       `gorm:"foreignKey:LimitCreatedBy;references:UserID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
-	LimitCreatedAt time.Time  `gorm:"autoCreateTime" json:"limit_created_at"`
-	LimitEditedBy  *uint      `json:"limit_edited_by"`
-	EditedByUser   *User      `gorm:"foreignKey:LimitEditedBy;references:UserID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
-	LimitEditedAt  *time.Time `json:"limit_edited_at"`
+	LimitID              uint       `gorm:"primaryKey" json:"limit_id"`
+	LimitNIK             string     `gorm:"not null" json:"limit_nik"`
+	NIKCustomer          Customer   `gorm:"foreignKey:LimitNIK;references:CustomerNIK;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	LimitTenor           int        `gorm:"not null" json:"limit_tenor"`
+	LimitAmount          float64    `gorm:"not null" json:"limit_amount"`
+	LimitUsedAmount      float64    `gorm:"not null" json:"limit_used_amount"`
+	LimitRemainingAmount float64    `gorm:"not null" json:"limit_remaining_amount"`
+	LimitCreatedBy       uint       `gorm:"not null" json:"limit_created_by"`
+	CreatedByUser        User       `gorm:"foreignKey:LimitCreatedBy;references:UserID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	LimitCreatedAt       time.Time  `gorm:"autoCreateTime" json:"limit_created_at"`
+	LimitEditedBy        *uint      `json:"limit_edited_by"`
+	EditedByUser         *User      `gorm:"foreignKey:LimitEditedBy;references:UserID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	LimitEditedAt        *time.Time `json:"limit_edited_at"`
 }
